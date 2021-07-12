6449d0b792544c7acba14bb279b160e2aa2b2517
Gian <marattagian15@gmail.com>
2021-07-12 20:35:56 -0300

    handler.go working correctly, yaml parser and map builder implemented

--- handler.go
+++ handler.go
@@ -2,14 +2,10 @@ package urlshort
 
 import (
 	"net/http"
+
+	"gopkg.in/yaml.v2"
 )
 
-// MapHandler will return an http.HandlerFunc (which also
-// implements http.Handler) that will attempt to map any
-// paths (keys in the map) to their corresponding URL (values
-// that each key in the map points to, in string format).
-// If the path is not provided in the map, then the fallback
-// http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		path := r.URL.Path
@@ -21,22 +17,6 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 	}
 }
 
-// YAMLHandler will parse the provided YAML and then return
-// an http.HandlerFunc (which also implements http.Handler)
-// that will attempt to map any paths to their corresponding
-// URL. If the path is not provided in the YAML, then the
-// fallback http.Handler will be called instead.
-//
-// YAML is expected to be in the format:
-//
-//     - path: /some-path
-//       url: https://www.some-url.com/demo
-//
-// The only errors that can be returned all related to having
-// invalid YAML data.
-//
-// See MapHandler to create a similar http.HandlerFunc via
-// a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
 	pathsUrls, err := parseYAML(yml)
 	if err != nil {
@@ -46,6 +26,24 @@ func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
 	return MapHandler(pathsToUrls, fallback), nil
 }
 
-func parseYAML() {}
+func buildMap(parsedPaths []pathToUrl) map[string]string {
+	mapedUrls := make(map[string]string)
+	for _, parsedValue := range parsedPaths {
+		mapedUrls[parsedValue.Path] = parsedValue.Url
+	}
+	return mapedUrls
+}
 
-func buildMap() {}
+func parseYAML(byteYAML []byte) ([]pathToUrl, error) {
+	var pathToUrl []pathToUrl
+	err := yaml.Unmarshal(byteYAML, &pathToUrl)
+	if err != nil {
+		return nil, err
+	}
+	return pathToUrl, nil
+}
+
+type pathToUrl struct {
+	Path string `yaml:"path"`
+	Url  string `yaml:"url"`
+}
